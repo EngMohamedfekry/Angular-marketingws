796ecd2b589670dfa282a560d52778250144ac9e
EngMohamedfekry <118119096+EngMohamedfekry@users.noreply.github.com>
2023-10-02 16:18:23 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { AdvertisngComponent } from './Components/advertisng/advertisng.component';
+import { ContactusComponent } from './Components/contactus/contactus.component';
+import { ElectioncampaignsComponent } from './Components/electioncampaigns/electioncampaigns.component';
+import { MobileappComponent } from './Components/mobileapp/mobileapp.component';
+import { WebsitedesignComponent } from './Components/websitedesign/websitedesign.component';
+import { WhousComponent } from './Components/whous/whous.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"ad",component:AdvertisngComponent},
+  {path:"contact-us",component:ContactusComponent},
+  {path:"ele",component:ElectioncampaignsComponent},
+  {path:"mob",component:MobileappComponent},
+  {path:"web-desgin",component:WebsitedesignComponent},
+  {path:"whous",component:WhousComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
